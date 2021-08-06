55bd9906bc30f4ce46b1b98903b4bcdf083b9536
vamcdumpeta <88522561+vamcdumpeta@users.noreply.github.com>
2021-08-06 18:31:36 +0530

    check

---
+++ Git hub practice/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Git hub practice
+//
+//  Created by Vamshi on 06/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ Git hub practiceTests/Git_hub_practiceTests.swift
@@ -0,0 +1,33 @@
+//
+//  Git_hub_practiceTests.swift
+//  Git hub practiceTests
+//
+//  Created by Vamshi on 06/08/21.
+//
+
+import XCTest
+@testable import Git_hub_practice
+
+class Git_hub_practiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
